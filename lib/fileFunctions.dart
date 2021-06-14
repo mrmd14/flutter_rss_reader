7c026aa1cd19d82e6ce59ae2c62b2235f10757c6
Sajad <dayan.acorn@gmail.com>
2021-06-14 22:56:13 +0430

    fileFunctions.dart added

---
+++ lib/fileFunctions.dart
@@ -0,0 +1,148 @@
+import 'main.dart';
+import 'package:localstorage/localstorage.dart';
+
+import 'package:webfeed/webfeed.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+const String SITE_ITEM_KEY = "siteKey";
+const String SITE_FILE_KEY = "siteFileKey";
+
+class printData {
+  bool ready = true;
+  Future<void> PData(SaveData saver) async {
+    ready = false;
+    saver.getData();
+    ready = await saver.storage.ready;
+    print(ready);
+    print('getttinnnnnnnnnnnnnnnnng dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    sites = saver.list.items;
+  }
+}
+
+class site {
+  String siteAdress;
+  String siteDescription;
+
+  site({this.siteAdress, this.siteDescription}) {
+    numberOfSites++;
+  }
+
+  toJSONEncodable() {
+    Map<String, String> m = new Map();
+
+    m['siteAdress'] = siteAdress;
+    m['siteDescription'] = siteDescription;
+
+    return m;
+  }
+
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class siteList {
+  List<site> items = [];
+
+  toJSONEncodable() {
+    return items.map((item) {
+      return item.toJSONEncodable();
+    }).toList();
+  }
+}
+
+class SaveData {
+  final siteList list = new siteList();
+  final LocalStorage storage = new LocalStorage(SITE_FILE_KEY);
+  bool ready = true;
+
+  void saveToStorage() {
+    storage.setItem(SITE_ITEM_KEY, list.toJSONEncodable());
+  }
+
+  Future<void> getData() async {
+    ready = false;
+    ready = await storage.ready;
+
+    var res_list = storage.getItem(SITE_ITEM_KEY);
+
+    list.items = List<site>.from(
+      (res_list as List).map(
+        (item) => site(
+          siteAdress: item['siteAdress'],
+          siteDescription: item['siteDescription'],
+        ),
+      ),
+    );
+  }
+}
+
+class RSS_item {
+  RssItem item;
+  String site_title;
+
+  RSS_item(_item, _site_title) {
+    item = _item;
+    site_title = _site_title;
+  }
+}
+
+class RSS_reader {
+  Future<String> Get_response(String _url, String path) async {
+    var url = Uri.http(_url, path);
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      return '${response.statusCode}';
+    }
+  }
+
+  Future<dynamic> Get_web_feed_items(String _rss_adr) async {
+    final uri = Uri.parse(_rss_adr);
+    var raw = await Get_response(uri.host, uri.path);
+    var channel = RssFeed.parse(raw);
+    return (channel.items);
+  }
+
+  String Get_rss(String _raw_html) {
+    String Href = 'href="';
+    var index = _raw_html.indexOf('rss+xml"');
+    if (index == -1) {
+      return '-1';
+    }
+    //found
+    var raw_cut = _raw_html.substring(index);
+    var begin = raw_cut.indexOf(Href) + Href.length;
+    var end = begin;
+    while (end < raw_cut.length && raw_cut[end] != '"') end = end + 1;
+    return raw_cut.substring(begin, end);
+  }
+
+  Future<dynamic> Get_website_rss_items(String add) async {
+    var raw = await Get_response(add, '');
+    var rss_adr = this.Get_rss(raw);
+
+    if (rss_adr != '-1') {
+      // return this.Get_web_feed_items(rss_adr);
+      var feed_items = await Get_web_feed_items(rss_adr);
+      return (feed_items);
+    }
+  }
+
+  Future<dynamic> Get_items(List<site> _adr_list) async {
+    var res = List<dynamic>();
+
+    for (var site_adr in _adr_list) {
+      var site_items = await Get_website_rss_items(site_adr.siteAdress);
+      for (var item in site_items) {
+        res.add(RSS_item(item, site_adr.siteDescription));
+      }
+    }
+    //sort res
+    res.sort((a, b) => b.item.pubDate.compareTo(a.item.pubDate));
+    return res;
+  }
+}
